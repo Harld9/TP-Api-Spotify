ca0462187a7e0a29ef8d5e3e8bc72b35591f09cc
Harld9 <harold.francois@ynov.com>
2025-11-20 18:14:13 +0100

    feat functions.go : Ajout fonction Refresh Token

--- functions/functions.go
+++ functions/functions.go
@@ -1,1 +1,62 @@
 package functions
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+	"time"
+)
+
+type ApiData struct {
+	TokenType   string `json:"token_type"`
+	AccessToken string `json:"access_token"`
+}
+
+func RefreshToken() {
+	// URL de L'API
+	urlApi := "https://accounts.spotify.com/api/token"
+	urlEnd := "client_id=c235f728d69640e5b067e81dc4af6c87&client_secret=38dc2a7cc1b642bdaf81f6fdbcf2ebca&grant_type=client_credentials"
+
+	// Initialisation du client HTTP qui va émettre/demander les requêtes
+	httpClient := http.Client{
+		Timeout: time.Second * 2, // Timeout apres 2sec
+	}
+
+	// Création de la requête HTTP vers L'API avec initialisation de la methode HTTP, la route et le corps de la requête
+	req, errReq := http.NewRequest(http.MethodPost, urlApi, strings.NewReader(urlEnd))
+	if errReq != nil {
+		fmt.Println("Oupss, une erreur est survenue : ", errReq.Error())
+		return
+	}
+
+	// Ajout d'une métadonnée dans le header, User_Agent permet d'identifier l'application, système ....
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	// Execution de la requête HTTP vars L'API
+	res, errResp := httpClient.Do(req)
+	if errResp != nil {
+		fmt.Println("Oupss, une erreur est survenue : ", errResp.Error())
+		return
+	}
+
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+
+	// Lecture et récupération du corps de la requête HTTP
+	body, errBody := io.ReadAll(res.Body)
+	if errBody != nil {
+		fmt.Println("Oupss, une erreur est survenue : ", errBody.Error())
+	}
+
+	// Déclaration de la variable qui va contenir les données
+	var decodeData ApiData
+
+	// Decodage des données en format JSON et ajout des donnée à la variable: decodeData
+	json.Unmarshal(body, &decodeData)
+
+	// Affichage des données
+	fmt.Println(decodeData)
+}
